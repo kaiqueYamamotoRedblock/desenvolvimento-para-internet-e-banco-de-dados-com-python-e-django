27cd2edbc887855b64d3c15ad3dd4ca1f8d9916c
Kaique Yamamoto | Full Stack Developer <kaique@redblock.com.br>
2023-11-19 17:49:08 -0300

    Criado relacionamento dentro da tabela de user

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from core.models import Evento
+
+class EventosAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'data_evento', 'data_criacao']
+
 # Register your models here.
+admin.site.register(Evento, EventosAdmin)
