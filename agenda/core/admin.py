e6a359aad3c17cc98e88fd61a5fbb05371357eb3
Kaique Yamamoto | Full Stack Developer <kaique@redblock.com.br>
2023-11-19 18:18:30 -0300

    Basico para estar criando e estar vendo a agenda.html

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from core.models import Evento
 
 class EventosAdmin(admin.ModelAdmin):
-    list_display = ['titulo', 'data_evento', 'data_criacao']
+    list_display = ['titulo', 'data_evento', 'data_criacao', 'usuario']
+    list_filter = ['usuario', 'titulo', 'data_evento', 'data_criacao']
 
 # Register your models here.
 admin.site.register(Evento, EventosAdmin)
--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from core.models import Evento
 
 # Create your views here.
+def list_events(request):
+    evento = Evento.objects.all()
+    dados = { 'eventos': evento}
+    return render(request, 'agenda.html', dados)
